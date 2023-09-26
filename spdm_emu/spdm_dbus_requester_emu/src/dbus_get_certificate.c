fdc98be9d18db5e02b50eaecc13122cc99d9193a
Zhichuang Sun <zhichuang@google.com>
2023-09-26 10:41:22 -0700

    add more info on get_measurements
    Signed-off-by: Zhichuang Sun <zhichuang@google.com>

--- spdm_emu/spdm_dbus_requester_emu/src/dbus_get_certificate.c
+++ spdm_emu/spdm_dbus_requester_emu/src/dbus_get_certificate.c
@@ -182,7 +182,7 @@ libspdm_return_t dbus_get_certificate(const char *fname) {
     cert_chain_size = sizeof(cert_chain);
     libspdm_zero_mem(cert_chain, sizeof(cert_chain));
 
-    // Get Digest
+    // Update slot_mask with Get Digest
     status = libspdm_get_digest(context, NULL, &slot_mask,
                                 total_digest_buffer);
     if (LIBSPDM_STATUS_IS_ERROR(status)) {
@@ -196,6 +196,7 @@ libspdm_return_t dbus_get_certificate(const char *fname) {
 
     cert_chain_buffer_size = cert_chain_size;
 
+    //TODO When slot_it != 0, should we create a certificate for each slot?
     if (slot_id != 0xFF) {
         printf("DBUG ONLY: slot_id:%u m_other_slot_id:%u \n", slot_id, m_other_slot_id);
         if (slot_id == 0) {
@@ -223,6 +224,7 @@ libspdm_return_t dbus_get_certificate(const char *fname) {
     }
 
     /**
+     * Extract DER formatted cert data from spdm response.
      * cert_chain format
      * | Length(2) | Reserved(2) | RootHash(H) | Certificates |
      */
@@ -234,12 +236,15 @@ libspdm_return_t dbus_get_certificate(const char *fname) {
         goto done;
     }
 
+    // Debug print only
     printf("cert_chain:%p, der_cert:%p, hash_size:%lu, cert_size:%lu, first byte: %02x\n",
             cert_chain, cert_chain + 4 + hash_size, hash_size, cert_chain_size, cert_chain[4+hash_size]);
+
+    // Only store the cert data.
     fwrite(cert_chain + 4 + hash_size, sizeof(uint8_t), cert_chain_size - 4 - hash_size, fptr);
 
-    printf("========== %s ===========\n",__func__);
-    dump_certificate(cert_chain, cert_chain_size);
+    // printf("========== %s ===========\n",__func__);
+    // dump_certificate(cert_chain, cert_chain_size);
 
     fclose(fptr);
 #endif /*(LIBSPDM_ENABLE_CAPABILITY_CERT_CAP && LIBSPDM_ENABLE_CAPABILITY_CHAL_CAP)*/
@@ -247,16 +252,27 @@ libspdm_return_t dbus_get_certificate(const char *fname) {
     return LIBSPDM_STATUS_SUCCESS;
 }
 
+// TODO: provide interface for dbus daemon to query about the meta data of the GET_MEAUREMENTS
+// For example, SPDM Version | HashingAlgorithm | SigningAlgorithm | Certificate
 
 /**
  * This function is called by the dbus daemon to get signed measurements from
  * device. 
+ * @slot_id: slot Id of the certificate to be used for signing the measurements.
+ * @nonce: a 32 byte nonce.
+ * @indices: an array of index for the measurement blocks to be measured. 
+ * @indices_len: length of indices array. 
  */ 
-libspdm_return_t dbus_get_certificate(const char *fname) {
+libspdm_return_t dbus_get_signed_measurements(size_t slot_id, uint8_t* nonce, size_t* indices, size_t indices_len) {
+    void *context;
     libspdm_return_t status;
+
+    uint8_t i;
+    uint8_t number_of_blocks;
     uint8_t number_of_block;
-    uint32_t measurement_record_length;
-    uint8_t measurement_record[LIBSPDM_MAX_MEASUREMENT_RECORD_SIZE];
+    uint32_t one_measurement_record_length;
+    uint8_t one_measurement_record[LIBSPDM_MAX_MEASUREMENT_RECORD_SIZE];
+    uint8_t all_measurement_records[8*LIBSPDM_MAX_MEASUREMENT_RECORD_SIZE]; /* TODO: 32KB or ? */
     uint8_t request_attribute;
     uint8_t requester_nonce_in[SPDM_NONCE_SIZE];
     uint8_t requester_nonce[SPDM_NONCE_SIZE];
@@ -264,13 +280,129 @@ libspdm_return_t dbus_get_certificate(const char *fname) {
     uint8_t opaque_data[SPDM_MAX_OPAQUE_DATA_SIZE];
     size_t opaque_data_size;
 
-    request_attribute = SPDM_GET_MEASUREMENTS_REQUEST_ATTRIBUTES_GENERATE_SIGNATURE;
-
-    measurement_record_length = sizeof(measurement_record);
-
-    for (int index = 0; index < SPDM_NONCE_SIZE; index++) {
-        requester_nonce_in[index] = 0x5c;
-        requester_nonce[index] = 0x00;
-        responder_nonce[index] = 0x00;
+    if (!nonce || !indices || indices_len == 0 || slot_id < 0) {
+        printf("Unexpected parameters : %s\n", __func__);
+        status = LIBSPDM_STATUS_INVALID_PARAMETER;
+        goto done;
     }
+
+    // request_attribute = SPDM_GET_MEASUREMENTS_REQUEST_ATTRIBUTES_GENERATE_SIGNATURE;
+    // measurement_record_length = sizeof(measurement_record);
+
+    // for (int index = 0; index < SPDM_NONCE_SIZE; index++) {
+    //     requester_nonce_in[index] = nonce[index];
+    //     requester_nonce[index] = 0x00;
+    //     responder_nonce[index] = 0x00;
+    // }
+
+    context = m_spdm_context;
+
+    request_attribute = m_use_measurement_attribute;
+
+    /**
+     * query the total number of blocks available and check whether the requested
+     * indices are within the range.
+     */ 
+    status = libspdm_get_measurement(
+        context, NULL, request_attribute,
+        SPDM_GET_MEASUREMENTS_REQUEST_MEASUREMENT_OPERATION_TOTAL_NUMBER_OF_MEASUREMENTS,
+        m_use_slot_id & 0xF, NULL, &number_of_blocks, NULL, NULL);
+    if (LIBSPDM_STATUS_IS_ERROR(status)) {
+        return status;
+    }
+    LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "number_of_blocks - 0x%x\n",
+                   number_of_blocks));
+    received_number_of_block = 0;
+
+    for (i = 0; i < indices_len; i++) {
+        /* 1. check given index and make sure it is valid, index start from 1, 0 is reserved see SPDM spec. */
+        if (indices[i] <= 0 && indices[i] > number_of_blocks) {
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "ERR: indices[%u] = %u is out of range (1, .., #BLOCKS %u)\n",
+                   i, indices[i], number_of_blocks));
+            break;
+        }
+
+        /* 2. query measurement one by one
+         * get signature in last message only.
+         *
+         * Note, according to SPDM Spec 1.3
+         * (see https://www.dmtf.org/sites/default/files/standards/documents/DSP0274_1.3.0.pdf, P116, L516)
+         * Signature = SPDMsign(PrivKey, L1, "measurements signing");
+         * Where L1/L2 = Concatenate(VCA, GET_MEASUREMENTS_REQUEST1, MEASUREMENTS_RESPONSE1, ...,
+         *       GET_MEASUREMENTS_REQUESTn-1, MEASUREMENTS_RESPONSEn-1,
+         *       GET_MEASUREMENTS_REQUESTn, MEASUREMENTS_RESPONSEn)
+         * REQ1 - REQn-1 no signature required
+         * REQn signature required
+         * We return the whole L2 back to Notar for verification.
+         */
+        one_measurement_record_length = sizeof(one_measurement_record);
+        if (i == indices_len - 1) {
+            /* generate signature with designated nonce */
+            request_attribute = m_use_measurement_attribute |
+                                SPDM_GET_MEASUREMENTS_REQUEST_ATTRIBUTES_GENERATE_SIGNATURE;
+
+            status = libspdm_get_measurement_ex(
+                context, NULL, request_attribute,
+                indices[i], slot_id & 0xF, NULL, &number_of_block,
+                &measurement_record_length, one_measurement_record,
+                requester_nonce_in, requester_nonce, responder_nonce,
+                opaque_data, &opaque_data_size);
+
+            // TODO some check:
+       	    //  assert(requester_nonce_in[index] == requester_nonce[index]);
+       	    //  assert(opaque_data_size, strlen("libspdm"));
+       	    //  assert(opaque_data == "libspdm");
+        } else {
+ 			status = libspdm_get_measurement(
+ 			    context, NULL, request_attribute,
+ 			    indices[i], slot_id & 0xF, NULL, &number_of_block,
+ 			    &one_measurement_record_length, one_measurement_record);
+        }
+
+ 		if (LIBSPDM_STATUS_IS_ERROR(status)) {
+ 		    continue;
+ 		}
+
+        received_number_of_block += 1;
+    }
+
+    if (received_number_of_block != indices_len) {
+        status = LIBSPDM_STATUS_INVALID_STATE_PEER;
+        goto done;
+    }
+
+    /* Base64 encode the measurements */
+    // TODO
+	// See library//spdm_requester_lib/libspdm_req_get_measurements.c on how to get everything for
+	//   component_integrity.cpp 
+    //      ComponentIntegrity::spdmGetSignedMeasurements(
+    //          std::vector<size_t> measurementIndices,
+    //          std::string nonce,
+    //          size_t slotId)
+	// For example:
+	//   pubkey: spdm_context->connection_info.peer_used_cert_chain[slot_id].leaf_cert_public_key;
+	//   l1l2:l1l2_buffer = libspdm_get_managed_buffer(&l1l2);
+	//   Signature:
+	//   SPDM_VERSION: spdm_context->connection_info.version
+    //   SIGNALG: spdm_context->connection_info.algorithm.base_asym_algo
+    //   HASHALG: spdm_context->connection_info.algorithm.base_hash_algo
+	//
+	// See library//spdm_requester_lib/libspdm_req_get_measurements.c L336 on how to extract signature from response
+	//     measurement_record_data_length = libspdm_read_uint24(spdm_response->measurement_record_length);
+	//     measurement_record_data = (void *)(spdm_response + 1);
+    //	   ptr = measurement_record_data + measurement_record_data_length;
+    //	   nonce = ptr;
+    //	   LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "nonce (0x%x) - ", SPDM_NONCE_SIZE));
+    //	   LIBSPDM_INTERNAL_DUMP_DATA(nonce, SPDM_NONCE_SIZE);
+    //	   LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "\n"));
+    //     ptr += SPDM_NONCE_SIZE;
+    //     opaque_length = libspdm_read_uint16((const uint8_t *)ptr);
+    //     ptr += sizeof(uint16_t);
+    //     ptr += opaque_length;
+    //     signature = ptr;
+
+    return LIBSPDM_STATUS_SUCCESS;
+
+done:
+    return status;
 }
